6f835733bc28150ecff345c59a067efffae3e369
Ben Jolas <benjolas@Mac-mini-von-Ben.local>
2026-05-22 10:55:15 +0200

    Testing real app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import { redirect } from 'next/navigation';
-
 export default function Page() {
-  redirect('/beta');
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-red-600 text-white">
+      <h1 className="text-9xl font-black">
+        APP PAGE REAL 🔥
+      </h1>
+    </main>
+  );
 }
