31b183960d609d7adbf5335173f7931f2e34528c
Ben Jolas <benjolas@Mac-mini-von-Ben.local>
2026-05-22 10:52:35 +0200

    Testing real home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,9 @@
-import HomeClient from "./home-client";
-
-export default function Page() {
-  return <HomeClient />;
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-9xl font-black">
+        PAGE TSX WORKS 🔥
+      </h1>
+    </main>
+  );
 }
