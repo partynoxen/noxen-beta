4352d4c4884c5b77e6ce6d62a4fe4e3059b5fec7
Ben Jolas <benjolas@Mac-mini-von-Ben.local>
2026-05-22 10:57:21 +0200

    Testing root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import "./globals.css";
-
 export default function RootLayout({
   children,
 }: {
@@ -7,7 +5,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
-      <body>{children}</body>
+      <body
+        style={{
+          background: "red",
+          color: "white",
+          fontSize: "120px",
+          fontWeight: "900",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+        }}
+      >
+        LAYOUT WORKS 🔥
+      </body>
     </html>
   );
 }
